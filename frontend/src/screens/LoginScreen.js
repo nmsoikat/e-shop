02f90a472931d8e5c4db8af701dde9dad8fd598b
nmsoikat <nursaykat@gmail.com>
2021-11-13 20:18:52 +0600

    implemnt redux for user and login-screen

---
+++ frontend/src/screens/LoginScreen.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { Form, Button, Row, Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { login } from '../actions/userAction'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import FormContainer from '../components/FormContainer'
+
+function LoginScreen({ location }) {
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
+
+  const redirect = location.search ? location.search.split('=')[1] : '/'
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+  }
+
+  return (
+    <FormContainer>
+      <h1>Sign In Here</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId="email">
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter you email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter you password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Button type="submit" variant="primary" className="mt-3">
+          Sign In
+        </Button>
+      </Form>
+
+      <Row className="py-4">
+        <Col>
+          New Customer?
+          <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>
+            Register
+          </Link>
+        </Col>
+      </Row>
+    </FormContainer>
+  )
+}
+
+export default LoginScreen
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,34 +1,42 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 import {
   productListReducer,
   productDetailsReducer,
-} from "./reducers/productReducers";
-import { cartReducer } from "./reducers/cartReducers";
+} from './reducers/productReducers'
+import { cartReducer } from './reducers/cartReducers'
+import { userReducers } from './reducers/userReducers'
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
-});
+  userLogin: userReducers,
+})
 
-// @ load cart data from local-storage
-const cartDataFromLocalStorage = localStorage.getItem("cartItems")
-  ? JSON.parse(localStorage.getItem("cartItems"))
-  : [];
+// @load cart data from local-storage
+const cartDataFromLocalStorage = localStorage.getItem('cartItems')
+  ? JSON.parse(localStorage.getItem('cartItems'))
+  : []
+
+// @current login user
+const userInfoFromLocalStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null
 
 const initialState = {
   cart: { cartItems: cartDataFromLocalStorage },
-};
+  userLogin: { userInfo: userInfoFromLocalStorage },
+}
 
-const middleware = [thunk];
+const middleware = [thunk]
 
 const store = createStore(
   reducer,
   initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
-);
+  composeWithDevTools(applyMiddleware(...middleware)),
+)
 
-export default store;
+export default store
